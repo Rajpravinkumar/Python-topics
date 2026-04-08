b7156441a6f9074a5de5d9fd02fcb00f7c63bc58
Rajpravinkumar <rajpravinkumaras@gmail.com>
2026-04-08 21:54:41 +0530

    display statement

---
+++ python.py
@@ -0,0 +1,19 @@
+# 001.Display Statement
+print("Welcome to Python ")
+print('Happy Learning of python')
+print("Raja's")
+print('"Hardwork"')
+print("Raja's", '"Hardwork"')
+print("10","20","30", sep = "|")
+print('''
+
+Leave Letter
+      12/12/24
+
+From
+      XXXX
+      yyyy      
+''')
+print("Today is \n\t Thursday")
+print("Thursday")
+print("#" * 30)
