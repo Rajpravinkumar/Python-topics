28c1f28ab0980273fdcca226ff063ef0a0c90d44
Rajpravinkumar <rajpravinkumaras@gmail.com>
2026-04-08 22:03:34 +0530

    Assignment statement and basic data type

--- README.md
+++ README.md
@@ -19,3 +19,23 @@ From
 print("Today is \n\t Thursday")
 print("Thursday")
 print("#" * 30)# Python-topics
+
+# Assignment statement and basic data type
+
+# 002.Assignment statement and basic data type
+
+a = 10 #assignment statement - allocates memory space for a and store data 10 in a 
+print(a)
+print(type(a)) # int
+
+b = 10.34
+print(b)
+print(type(b)) # float
+
+c = "Hello"
+print(c)
+print(type(c)) # string
+
+d = True
+print(d)
+print(type(d))
--- python.py
+++ python.py
@@ -1,19 +1,16 @@
-# 001.Display Statement
-print("Welcome to Python ")
-print('Happy Learning of python')
-print("Raja's")
-print('"Hardwork"')
-print("Raja's", '"Hardwork"')
-print("10","20","30", sep = "|")
-print('''
+# 002.Assignment statement and basic data type
+a = 10 #assignment statement - allocates memory space for a and store data 10 in a 
+print(a)
+print(type(a)) # int
 
-Leave Letter
-      12/12/24
+b = 10.34
+print(b)
+print(type(b)) # float
 
-From
-      XXXX
-      yyyy      
-''')
-print("Today is \n\t Thursday")
-print("Thursday")
-print("#" * 30)
+c = "Hello"
+print(c)
+print(type(c)) # string
+
+d = True
+print(d)
+print(type(d))
